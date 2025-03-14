3b99b702b065410190bf2150bfaaea0910973202
ctravizanutto <ctravizanutto@gmail.com>
2025-03-13 23:23:08 -0300

    fix: breadcrumbs

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -4,6 +4,25 @@ import DriveContents from "../../drive-contents"
 import { eq } from "drizzle-orm"
 
 
+async function getAllParents(folderId: number) {
+  const parents = []
+  let currentFolderId: number | null = folderId
+  while (currentFolderId !== null) {
+    const folder = await db
+      .selectDistinct()
+      .from(foldersSchema)
+      .where(eq(foldersSchema.id, currentFolderId))
+
+    if (!folder[0]) {
+      throw new Error("Folder not found")
+    }
+    parents.unshift(folder[0])
+    currentFolderId = folder[0]?.parent
+  }
+
+  return parents;
+}
+
 export default async function GoogleDriveClone(props: { params: Promise<{ folderId: string }> }) {
   const params = await props.params
   
@@ -12,12 +31,23 @@ export default async function GoogleDriveClone(props: { params: Promise<{ folder
     return <div>Invalid folder ID</div>
   }
 
-  const files = await db.select().from(filesSchema)
-  const folders = await db
+  const begin = new Date()
+
+  const filesPromise = db
+    .select()
+    .from(filesSchema)
+    .where(eq(filesSchema.parent, parsedFolderId))
+  const foldersPromise = db
     .select()
     .from(foldersSchema)
-    .where(eq(foldersSchema.parent, parsedFolderId))
+    .where(eq(foldersSchema.parent , parsedFolderId))
 
-  return <DriveContents files={files} folders={folders} />
+  const parentsPromise = getAllParents(parsedFolderId)
+  const [folders, files, parents] = await Promise.all([foldersPromise, filesPromise, parentsPromise])
+
+  const end = new Date()
+  console.log(`Time taken to fetch data: ${end.getTime() - begin.getTime()}ms`)
+
+  return <DriveContents files={files} folders={folders} parents={parents}/>
 }
 
