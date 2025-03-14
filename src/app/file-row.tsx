46d6fbe3e04cc0892b455f8049ecdbb837e8c87d
ctravizanutto <ctravizanutto@gmail.com>
2025-03-13 23:24:24 -0300

    better names for schema

--- src/app/file-row.tsx
+++ src/app/file-row.tsx
@@ -1,8 +1,8 @@
 import { FileIcon, Folder as FolderIcon } from "lucide-react"
 import Link from "next/link"
-import type { files, folders } from "~/server/db/schema"
+import type { files_table, folders_table } from "~/server/db/schema"
 
-export function FileRow(props: { file: typeof files.$inferSelect }) {
+export function FileRow(props: { file: typeof files_table.$inferSelect }) {
     const { file } = props
     return (
         <li key={file.id} className="px-6 py-4 border-b border-gray-700 hover:bg-gray-750">
@@ -20,7 +20,7 @@ export function FileRow(props: { file: typeof files.$inferSelect }) {
     )
 }
 
-export function FolderRow(props: { folder: typeof folders.$inferSelect }) {
+export function FolderRow(props: { folder: typeof folders_table.$inferSelect }) {
     const { folder } = props
     return (
         <li key={folder.id} className="px-6 py-4 border-b border-gray-700 hover:bg-gray-750">
