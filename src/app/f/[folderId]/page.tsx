043d5fcf392ac4fab2921f8d9ca27d29d1d9d915
ctravizanutto <ctravizanutto@gmail.com>
2025-03-13 23:39:04 -0300

    fix: move queries to data layer

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,31 +1,9 @@
-import { db } from "~/server/db"
-import { files_table as filesSchema, folders_table as foldersSchema} from "~/server/db/schema"
 import DriveContents from "../../drive-contents"
-import { eq } from "drizzle-orm"
-
-
-async function getAllParents(folderId: number) {
-  const parents = []
-  let currentFolderId: number | null = folderId
-  while (currentFolderId !== null) {
-    const folder = await db
-      .selectDistinct()
-      .from(foldersSchema)
-      .where(eq(foldersSchema.id, currentFolderId))
-
-    if (!folder[0]) {
-      throw new Error("Folder not found")
-    }
-    parents.unshift(folder[0])
-    currentFolderId = folder[0]?.parent
-  }
-
-  return parents;
-}
+import { QUERIES } from "~/server/db/queries"
 
 export default async function GoogleDriveClone(props: { params: Promise<{ folderId: string }> }) {
   const params = await props.params
-  
+
   const parsedFolderId = parseInt(params.folderId)
   if (isNaN(parsedFolderId)) {
     return <div>Invalid folder ID</div>
@@ -33,21 +11,14 @@ export default async function GoogleDriveClone(props: { params: Promise<{ folder
 
   const begin = new Date()
 
-  const filesPromise = db
-    .select()
-    .from(filesSchema)
-    .where(eq(filesSchema.parent, parsedFolderId))
-  const foldersPromise = db
-    .select()
-    .from(foldersSchema)
-    .where(eq(foldersSchema.parent , parsedFolderId))
-
-  const parentsPromise = getAllParents(parsedFolderId)
-  const [folders, files, parents] = await Promise.all([foldersPromise, filesPromise, parentsPromise])
+  const [files, folders, parents] = await Promise.all([
+    QUERIES.getFiles(parsedFolderId), 
+    QUERIES.getFolders(parsedFolderId), 
+    QUERIES.getAllParentsForFolder(parsedFolderId)
+  ])
 
   const end = new Date()
   console.log(`Time taken to fetch data: ${end.getTime() - begin.getTime()}ms`)
 
-  return <DriveContents files={files} folders={folders} parents={parents}/>
+  return <DriveContents files={files} folders={folders} parents={parents} />
 }
-
