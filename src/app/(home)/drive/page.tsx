6461f663da39f34f6cb99e99d050e2c408bd20a5
ctravizanutto <ctravizanutto@gmail.com>
2025-03-15 18:37:15 -0300

    new sign-in page

--- src/app/(home)/drive/page.tsx
+++ src/app/(home)/drive/page.tsx
@@ -4,34 +4,34 @@ import { Button } from "~/components/ui/button";
 import { MUTATIONS, QUERIES } from "~/server/db/queries";
 
 export default async function DrivePage() {
-  const session = await auth();
+    const session = await auth();
 
-  if (!session.userId) {
-    return redirect("/sign-in");
-  }
+    if (!session.userId) {
+        return redirect("/sign-in");
+    }
 
-  const rootFolder = await QUERIES.getRootFolderForUser(session.userId);
+    const rootFolder = await QUERIES.getRootFolderForUser(session.userId);
 
-  if (!rootFolder) {
-    return (
-      <form
-        action={async () => {
-          "use server";
-          const session = await auth();
+    if (!rootFolder) {
+        return (
+            <form
+                action={async () => {
+                    "use server";
+                    const session = await auth();
 
-          if (!session.userId) {
-            return redirect("/sign-in");
-          }
+                    if (!session.userId) {
+                        return redirect("/sign-in");
+                    }
 
-          const rootFolderId = await MUTATIONS.onboardUser(session.userId);
+                    const rootFolderId = await MUTATIONS.onboardUser(session.userId);
 
-          return redirect(`/f/${rootFolderId}`);
-        }}
-      >
-        <Button>Create new Drive</Button>
-      </form>
-    );
-  }
+                    return redirect(`/f/${rootFolderId}`);
+                }}
+            >
+                <Button>Create new Drive</Button>
+            </form>
+        );
+    }
 
-  return redirect(`/f/${rootFolder.id}`);
+    return redirect(`/f/${rootFolder.id}`);
 }
